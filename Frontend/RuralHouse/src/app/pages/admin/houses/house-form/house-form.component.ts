55524876f20490bfad10524a871f4d18a444af2a
Joaquinrs05 <joaquinrs134@gmail.com>
2025-06-02 12:14:30 +0200

    formulario casas

---
+++ Frontend/RuralHouse/src/app/pages/admin/houses/house-form/house-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-house-form',
+  imports: [],
+  templateUrl: './house-form.component.html',
+  styleUrl: './house-form.component.scss'
+})
+export class HouseFormComponent {
+
+}
